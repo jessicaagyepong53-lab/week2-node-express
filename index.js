4a31778f2ad5157963a792ad31a213f4d50e319f
Agyepong Jessica <jessicaagyepong53@gmail.com>
2026-03-19 23:35:44 +0000

    Assignment

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// BONUS: Custom logger middleware
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/api', (req, res) => {
   res.json({ message: 'My Week 2 API!' });
 });
